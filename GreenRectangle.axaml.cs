37ecad66790dbc49e139c59abbd4cdb271ecfa32
Gary McGhee <gary@huey.co>
2023-11-02 20:56:28 +0800

    Initial commit

---
+++ GreenRectangle.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+
+namespace CustomControlPlay;
+
+public partial class GreenRectangle : Rectangle
+{
+    public GreenRectangle()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RedButton.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CustomControlPlay;
+
+public partial class RedButton : Button
+{
+    public RedButton()
+    {
+        InitializeComponent();
+    }
+}
